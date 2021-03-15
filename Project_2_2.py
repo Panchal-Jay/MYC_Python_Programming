a5488f46df55450394b4e9c40503cb153c7a89b5
JayPanchal1 <80104924+JayPanchal1@users.noreply.github.com>
2021-03-15 16:06:10 +0530

    Python Project_2_2
    code for given list of numbers which contains positive and negative both and from this code we get all positive numbers from list..

---
+++ Project_2_2.py
@@ -0,0 +1,21 @@
+# Task 2
+
+# Printing all positive numbers from given range or List
+
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+
+print("Positive Numbers of list1 : ")
+for num in list1:
+# condition for numbers if they are (>0) then they will be printed
+    if num >=0:
+        print(num)
+
+print("Positive Numbers of list2 : ")
+for num in list2:
+# condition for numbers if they are (>0) then they will be printed
+    if num >=0:
+        print(num)
+
+
+        
